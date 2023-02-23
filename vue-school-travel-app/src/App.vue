4aa34580af4338d2c6e71d3c5f331645c87e0896
Fred He <fhe@bayviewglen.ca>
2023-02-22 22:23:14 -0500

    Navigation Component Done

--- vue-school-travel-app/src/App.vue
+++ vue-school-travel-app/src/App.vue
@@ -1,25 +1,13 @@
 <script>
+import TheNavigation from "./components/TheNavigation.vue"
+export default{
+  components: {TheNavigation}
+}
 </script>
 
 <template>
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <a href="/about">A tag About</a>
-      <router-view></router-view>
+  <TheNavigation/>
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- vue-school-travel-app/src/router/index.js
+++ vue-school-travel-app/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory  } from "vue-router";
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
 
 const routes = [
     { path: "/", name: "Home", component: Home },
-    { path: "/about", name: "About", component: About }
+    { path: "/brazil", name: "brazil", component: ()=>import("../views/Brazil.vue")},
+    { path: "/hawaii", name: "hawaii", component: ()=>import("../views/Hawaii.vue")},
+    { path: "/jamaica", name: "jamaica", component: ()=>import("../views/Jamaica.vue")},
+    { path: "/panama", name: "panama", component: ()=>import("../views/Panama.vue")},
 ]
 
 const router = createRouter({
