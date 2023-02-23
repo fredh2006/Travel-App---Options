16719d057b0061122f06f57c447d91175a22e0e1
Fred He <fhe@bayviewglen.ca>
2023-02-22 20:47:21 -0500

    intial commit

---
+++ vue-school-travel-app/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<template>
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <a href="/about">A tag About</a>
+      <router-view></router-view>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
