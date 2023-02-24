c06b1ed07f6df53a7ea9ccd9694b1f2b70abeea9
Fred He <fhe@bayviewglen.ca>
2023-02-23 22:55:15 -0500

    Route Transitions Done

--- vue-school-travel-app/src/router/index.js
+++ vue-school-travel-app/src/router/index.js
@@ -1,17 +1,27 @@
-import { createRouter, createWebHistory  } from "vue-router";
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
-    { path: "/", name: "Home", component: Home },
-    { path: "/brazil", name: "brazil", component: ()=>import("../views/Brazil.vue")},
-    { path: "/hawaii", name: "hawaii", component: ()=>import("../views/Hawaii.vue")},
-    { path: "/jamaica", name: "jamaica", component: ()=>import("../views/Jamaica.vue")},
-    { path: "/panama", name: "panama", component: ()=>import("../views/Panama.vue")},
-]
+  { path: "/", name: "Home", component: Home },
+  {
+    path: "/destination/:id/:slug",
+    name: "destination.show",
+    component: () => import("../views/DestinationShow.vue"),
+    props: (route) => ({...route.params, id: parseInt(route.params.id) }),
+    children:[
+      {
+        path: ":experienceSlug",
+        name: "experience.show",
+        component: () => import("../views/ExperienceShow.vue"),
+        props: (route) => ({...route.params, id: parseInt(route.params.id) }),
+      }
+    ]
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
-export default router
+export default router;
