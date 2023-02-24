621243cdc24c31966db748e4a31230de32adac80
Fred He <fhe@bayviewglen.ca>
2023-02-24 08:02:02 -0500

    Route Guards Done

--- vue-school-travel-app/src/router/index.js
+++ vue-school-travel-app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import sourceData from "../data.json"
 
 const routes = [
   { path: "/", name: "Home", component: Home },
@@ -8,6 +9,15 @@ const routes = [
     name: "destination.show",
     component: () => import("../views/DestinationShow.vue"),
     props: (route) => ({...route.params, id: parseInt(route.params.id) }),
+    beforeEnter(to, from){
+      const exists = sourceData.destinations.find((destination)=>destination.id===parseInt(to.params.id))
+      if(!exists) return{
+        name: 'NotFound',
+        params: { pathMatch: to.path.split('/').slice(1) },
+        query: to.query,
+        hash: to.hash,
+      }
+    },
     children:[
       {
         path: ":experienceSlug",
@@ -16,6 +26,11 @@ const routes = [
         props: (route) => ({...route.params, id: parseInt(route.params.id) }),
       }
     ]
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: ()=>import('../views/NotFound.vue')
   },
 ];
 
